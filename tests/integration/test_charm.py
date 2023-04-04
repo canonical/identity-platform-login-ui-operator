660ecf3ce33473f05088472a50c81c8dd3d2d249
bencekov <bence.kovacs.business@gmail.com>
2023-04-04 10:53:37 -0600

    fix:fixed linting in tests/integration/test_charm.py

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -68,8 +68,6 @@ async def test_has_ingress(ops_test: OpsTest):
     # Get the traefik address and try to reach kratos
     public_address = await get_unit_address(ops_test, TRAEFIK_PUBLIC_APP, 0)
 
-    resp = requests.get(
-        f"http://{public_address}/{ops_test.model.name}-{APP_NAME}"
-    )
+    resp = requests.get(f"http://{public_address}/{ops_test.model.name}-{APP_NAME}")
 
     assert resp.status_code == 200
