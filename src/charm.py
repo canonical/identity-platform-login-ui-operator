8b14c2a9a430b6ea8e6da3553c4de3cc39725a4c
bencekov <122398123+bencekov@users.noreply.github.com>
2023-04-18 01:32:33 -0600

    Add hydra_endpoints  requirer and login_ui_endpoints provider to identity platform login ui operator (#16)
    * fix: Fixes for PR #13 comments
    
    * fix: Fixes for PR #16
    
    * fix: more fixes for PR #16
    
    * fix: fixed bug in login_ui_endpoints lib
    
    * fix: fixed comments in PR #16
    
    * fix: fixed integration test according to PR #16
    
    * fix:fixed linting in tests/integration/test_charm.py
    
    * updated login_ui_endpoints.py with LoginUIEndpointsRelationUnavailableError
    
    * fix:fixed linting spelling mistake in login_ui_endpoint.py
    
    * fix: Fixes for PR #16\n changed publish-lib workflow to release_lib workflow\n minor changes in login_ui_endpoints.py lib and charm\n updated unit tests
    
    * fix: Fixes for comments in PR #16
    
    * fix: fixed linting issue with last commit
    
    * fix: fixed typo in tests.yaml workflow
    
    * fix: Fixed issues for PR #16
    
    * fix:
    - changed pytest version to 0.26.0
    - fixed minor requests in PR #16
    
    * fix:
    - fixed issue with login_ui_endpoints library outlined in PR #16
    
    * fix: fixed issue with login_ui_endpoints library
    
    * fix: fixed typo in workflows/tests.yaml, and fixed issue with login_ui_endpoints library method not raising an error when there are too many connections of the same name
    
    * fix: minor fixes in login_ui_endpoints library
    
    * fix: fixed bug that prevented integration with kratos and hydra

--- src/charm.py
+++ src/charm.py
@@ -6,8 +6,16 @@
 
 """A Juju charm for Identity Platform Login UI."""
 import logging
-from typing import Optional
 
+from charms.hydra.v0.hydra_endpoints import (
+    HydraEndpointsRelationDataMissingError,
+    HydraEndpointsRelationMissingError,
+    HydraEndpointsRequirer,
+)
+from charms.identity_platform_login_ui_operator.v0.login_ui_endpoints import (
+    LoginUIEndpointsProvider,
+    LoginUINonLeaderOperationError,
+)
 from charms.kratos.v0.kratos_endpoints import (
     KratosEndpointsRelationDataMissingError,
     KratosEndpointsRequirer,
@@ -18,7 +26,7 @@ from charms.traefik_k8s.v1.ingress import (
     IngressPerAppRequirer,
     IngressPerAppRevokedEvent,
 )
-from ops.charm import CharmBase, ConfigChangedEvent, HookEvent, WorkloadEvent
+from ops.charm import CharmBase, ConfigChangedEvent, HookEvent, RelationEvent, WorkloadEvent
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
 from ops.pebble import ChangeError, Layer
@@ -37,6 +45,7 @@ class IdentityPlatformLoginUiOperatorCharm(CharmBase):
         super().__init__(*args)
         self._container_name = "login-ui"
         self._container = self.unit.get_container(self._container_name)
+        self._hydra_relation_name = "endpoint-info"
         self._kratos_relation_name = "kratos-endpoint-info"
 
         self.service_patcher = KubernetesServicePatch(
@@ -52,11 +61,21 @@ class IdentityPlatformLoginUiOperatorCharm(CharmBase):
         self.kratos_endpoints = KratosEndpointsRequirer(
             self, relation_name=self._kratos_relation_name
         )
+        self.hydra_endpoints = HydraEndpointsRequirer(
+            self, relation_name=self._hydra_relation_name
+        )
+        self.endpoints_provider = LoginUIEndpointsProvider(self)
 
         self.framework.observe(self.on.login_ui_pebble_ready, self._on_login_ui_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(
             self.on[self._kratos_relation_name].relation_changed, self._update_pebble_layer
+        )
+        self.framework.observe(
+            self.endpoints_provider.on.ready, self._update_login_ui_endpoint_relation_data
+        )
+        self.framework.observe(
+            self.on[self._hydra_relation_name].relation_changed, self._update_pebble_layer
         )
         self.framework.observe(self.ingress.on.ready, self._on_ingress_ready)
         self.framework.observe(self.ingress.on.revoked, self._on_ingress_revoked)
@@ -92,10 +111,14 @@ class IdentityPlatformLoginUiOperatorCharm(CharmBase):
     def _on_ingress_ready(self, event: IngressPerAppReadyEvent) -> None:
         if self.unit.is_leader():
             logger.info("This app's public ingress URL: %s", event.url)
+        self._update_pebble_layer(event)
+        self._update_login_ui_endpoint_relation_data(event)
 
     def _on_ingress_revoked(self, event: IngressPerAppRevokedEvent) -> None:
         if self.unit.is_leader():
             logger.info("This app no longer has ingress")
+        self._update_pebble_layer(event)
+        self._update_login_ui_endpoint_relation_data(event)
 
     @property
     def _login_ui_layer(self) -> Layer:
@@ -110,7 +133,7 @@ class IdentityPlatformLoginUiOperatorCharm(CharmBase):
                     "command": "identity_platform_login_ui",
                     "startup": "enabled",
                     "environment": {
-                        "HYDRA_ADMIN_URL": self.config.get("hydra_url"),
+                        "HYDRA_ADMIN_URL": self._get_hydra_endpoint_info(),
                         "KRATOS_PUBLIC_URL": self._get_kratos_endpoint_info(),
                         "PORT": APPLICATION_PORT,
                     },
@@ -119,16 +142,35 @@ class IdentityPlatformLoginUiOperatorCharm(CharmBase):
         }
         return Layer(pebble_layer)
 
-    def _get_kratos_endpoint_info(self) -> Optional[str]:
-        kratos_public_url = ""
+    def _get_kratos_endpoint_info(self) -> str:
         if self.model.relations[self._kratos_relation_name]:
             try:
                 kratos_endpoints = self.kratos_endpoints.get_kratos_endpoints()
                 kratos_public_url = kratos_endpoints["public_endpoint"]
+                return kratos_public_url
             except KratosEndpointsRelationDataMissingError:
                 logger.info("No kratos-endpoint-info relation data found")
+                return ""
+        return ""
 
-        return kratos_public_url
+    def _update_login_ui_endpoint_relation_data(self, event: RelationEvent) -> None:
+        endpoint = self.ingress.url if self.ingress.is_ready() else ""
+        try:
+            self.endpoints_provider.send_endpoints_relation_data(endpoint)
+            logger.info(f"Sending login ui endpoint info: endpoint - {endpoint}")
+        except LoginUINonLeaderOperationError:
+            logger.info("Non-leader unit can't update relation data")
+
+    def _get_hydra_endpoint_info(self) -> str:
+        hydra_url = ""
+        try:
+            hydra_endpoints = self.hydra_endpoints.get_hydra_endpoints()
+            hydra_url = hydra_endpoints["public_endpoint"]
+        except HydraEndpointsRelationDataMissingError:
+            logger.info("No hydra endpoint-info relation data found")
+        except HydraEndpointsRelationMissingError:
+            logger.info("No hydra endpoint-info relation found")
+        return hydra_url
 
 
 if __name__ == "__main__":  # pragma: nocover
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -10,7 +10,6 @@ from ops.model import ActiveStatus, WaitingStatus
 
 CONTAINER_NAME = "login-ui"
 TEST_PORT = "8080"
-TEST_HYDRA_URL = "http://hydra:port"
 
 
 def setup_ingress_relation(harness) -> int:
@@ -27,7 +26,7 @@ def setup_ingress_relation(harness) -> int:
     return relation_id
 
 
-def setup_kratos_relation(harness) -> None:
+def setup_kratos_relation(harness) -> int:
     relation_id = harness.add_relation("kratos-endpoint-info", "kratos")
     harness.add_relation_unit(relation_id, "kratos/0")
     harness.update_relation_data(
@@ -38,6 +37,21 @@ def setup_kratos_relation(harness) -> None:
             "public_endpoint": f"http://kratos-public-url:80/{harness.model.name}-kratos",
         },
     )
+    return relation_id
+
+
+def setup_hydra_relation(harness) -> int:
+    relation_id = harness.add_relation("endpoint-info", "hydra")
+    harness.add_relation_unit(relation_id, "hydra/0")
+    harness.update_relation_data(
+        relation_id,
+        "hydra",
+        {
+            "admin_endpoint": f"http://hydra-admin-url:80/{harness.model.name}-hydra",
+            "public_endpoint": f"http://hydra-public-url:80/{harness.model.name}-hydra",
+        },
+    )
+    return relation_id
 
 
 def test_not_leader(harness) -> None:
@@ -72,13 +86,11 @@ def test_install_can_not_connect(harness) -> None:
     assert harness.charm.unit.status == WaitingStatus("Waiting to connect to Login_UI container")
 
 
-def test_layer_updated_without_kratos_endpoint_info(harness) -> None:
+def test_layer_updated_without_any_endpoint_info(harness) -> None:
     """Test Pebble Layer after updates."""
     harness.set_leader(True)
     harness.set_can_connect(CONTAINER_NAME, True)
     harness.charm.on.login_ui_pebble_ready.emit(CONTAINER_NAME)
-
-    harness.update_config({"hydra_url": TEST_HYDRA_URL})
 
     expected_layer = {
         "summary": "login_ui layer",
@@ -90,7 +102,7 @@ def test_layer_updated_without_kratos_endpoint_info(harness) -> None:
                 "command": "identity_platform_login_ui",
                 "startup": "enabled",
                 "environment": {
-                    "HYDRA_ADMIN_URL": TEST_HYDRA_URL,
+                    "HYDRA_ADMIN_URL": "",
                     "KRATOS_PUBLIC_URL": "",
                     "PORT": TEST_PORT,
                 },
@@ -106,9 +118,7 @@ def test_layer_updated_with_kratos_endpoint_info(harness) -> None:
     harness.set_leader(True)
     harness.set_can_connect(CONTAINER_NAME, True)
     harness.charm.on.login_ui_pebble_ready.emit(CONTAINER_NAME)
-    setup_kratos_relation(harness)
-
-    harness.update_config({"hydra_url": TEST_HYDRA_URL})
+    kratos_relation_id = setup_kratos_relation(harness)
 
     expected_layer = {
         "summary": "login_ui layer",
@@ -120,8 +130,73 @@ def test_layer_updated_with_kratos_endpoint_info(harness) -> None:
                 "command": "identity_platform_login_ui",
                 "startup": "enabled",
                 "environment": {
-                    "HYDRA_ADMIN_URL": TEST_HYDRA_URL,
-                    "KRATOS_PUBLIC_URL": f"http://kratos-public-url:80/{harness.model.name}-kratos",
+                    "HYDRA_ADMIN_URL": "",
+                    "KRATOS_PUBLIC_URL": harness.get_relation_data(kratos_relation_id, "kratos")[
+                        "public_endpoint"
+                    ],
+                    "PORT": TEST_PORT,
+                },
+            }
+        },
+    }
+
+    assert harness.charm._login_ui_layer.to_dict() == expected_layer
+
+
+def test_layer_updated_with_hydra_endpoint_info(harness) -> None:
+    """Test Pebble Layer when relation data is in place."""
+    harness.set_leader(True)
+    harness.set_can_connect(CONTAINER_NAME, True)
+    harness.charm.on.login_ui_pebble_ready.emit(CONTAINER_NAME)
+    hydra_relation_id = setup_hydra_relation(harness)
+
+    expected_layer = {
+        "summary": "login_ui layer",
+        "description": "pebble config layer for identity platform login ui",
+        "services": {
+            CONTAINER_NAME: {
+                "override": "replace",
+                "summary": "identity platform login ui",
+                "command": "identity_platform_login_ui",
+                "startup": "enabled",
+                "environment": {
+                    "HYDRA_ADMIN_URL": harness.get_relation_data(hydra_relation_id, "hydra")[
+                        "public_endpoint"
+                    ],
+                    "KRATOS_PUBLIC_URL": "",
+                    "PORT": TEST_PORT,
+                },
+            }
+        },
+    }
+
+    assert harness.charm._login_ui_layer.to_dict() == expected_layer
+
+
+def test_layer_updated_with_endpoint_info(harness) -> None:
+    """Test Pebble Layer when relation data is in place."""
+    harness.set_leader(True)
+    harness.set_can_connect(CONTAINER_NAME, True)
+    harness.charm.on.login_ui_pebble_ready.emit(CONTAINER_NAME)
+    hydra_relation_id = setup_hydra_relation(harness)
+    kratos_relation_id = setup_kratos_relation(harness)
+
+    expected_layer = {
+        "summary": "login_ui layer",
+        "description": "pebble config layer for identity platform login ui",
+        "services": {
+            CONTAINER_NAME: {
+                "override": "replace",
+                "summary": "identity platform login ui",
+                "command": "identity_platform_login_ui",
+                "startup": "enabled",
+                "environment": {
+                    "HYDRA_ADMIN_URL": harness.get_relation_data(hydra_relation_id, "hydra")[
+                        "public_endpoint"
+                    ],
+                    "KRATOS_PUBLIC_URL": harness.get_relation_data(kratos_relation_id, "kratos")[
+                        "public_endpoint"
+                    ],
                     "PORT": TEST_PORT,
                 },
             }
