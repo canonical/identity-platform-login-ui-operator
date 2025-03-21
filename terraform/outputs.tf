e31034e3fbb07e864f9333c69f956f77b8f4697b
dushu <shu.du@canonical.com>
2025-03-20 20:24:54 -0400

    feat: add terraform module

---
+++ terraform/outputs.tf
@@ -0,0 +1,28 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
+  description = "The Juju application name"
+  value       = juju_application.login_ui.name
+}
+
+output "requires" {
+  description = "The Juju integrations that the charm requires"
+  value = {
+    ingress             = "ingress"
+    kratos-info         = "kratos-info"
+    hydra-endpoint-info = "hydra-endpoint-info"
+    logging             = "logging"
+    tracing             = "tracing"
+    receive-ca-cert     = "receive-ca-cert"
+  }
+}
+
+output "provides" {
+  description = "The Juju integrations that the charm provides"
+  value = {
+    ui-endpoint-info  = "ui-endpoint-info"
+    metrics-endpoint  = "metrics-endpoint"
+    grafana-dashboard = "grafana-dashboard"
+  }
+}
