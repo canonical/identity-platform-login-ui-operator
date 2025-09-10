49ba6b3fbfa43654636ed75a3e6e8f6373032ea2
shipperizer <alexcabb@gmail.com>
2025-09-10 10:26:56 +0100

    fixup! 918131d

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -59,9 +59,9 @@ async def test_build_and_deploy(ops_test: OpsTest, local_charm: Path) -> None:
 async def test_ingress_relation(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(
         apps=[TRAEFIK_PUBLIC_APP],
-        status="active",
         raise_on_blocked=True,
-        timeout=1000,
+        status="active",
+        timeout=5 * 60,
     )
 
 
